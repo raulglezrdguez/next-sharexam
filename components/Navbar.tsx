44a5239c0efb369bbac9c6a7e95db9e8d38a31f8
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2026-01-02 15:54:17 -0500

    logo image added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,27 @@
 import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Navbar() {
   const session = await auth();
 
   return (
-    <nav className="flex justify-between p-4 border-b">
-      <span>Sharexam</span>
+    <nav className="flex flex-row justify-between p-4 border-b">
+      <div className="flex flex-row justify-center align-middle items-center gap-2">
+        <Link href={"/"} className="mr-5 md:mr-10">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            className="min-w-12 object-cover"
+            width={50}
+            height={50}
+          />
+        </Link>
+        <span className="text-3xl">Sharexam</span>
+      </div>
       {session ? (
         <div className="flex gap-4 items-center">
-          <p>Hello, {session.user?.name}</p>
+          <p className="text-sm">{session.user?.name}</p>
           <form
             action={async () => {
               "use server";
@@ -25,7 +38,7 @@ export default async function Navbar() {
             await signIn("google");
           }}
         >
-          <button className="text-blue-500">Start Google session</button>
+          <button className="text-blue-500">Continue with Google</button>
         </form>
       )}
     </nav>
